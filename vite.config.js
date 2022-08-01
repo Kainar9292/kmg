f02441acc6564e151fac29cba633bd7d87af093c
Kainar <toleubaevkai@mail.ru>
2022-08-01 19:53:36 +0600

    fix: fix base url in vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://kainar9292.github.io/dist/",
+  base: "/dist/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
