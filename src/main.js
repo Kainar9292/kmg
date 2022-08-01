d61b16fcaa777153492bc9d865d2df3b21dbd15d
Kainar <toleubaevkai@mail.ru>
2022-08-01 19:12:22 +0600

    fix: fix imports in main.js file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import { createApp } from 'vue'
-import './scss/app.scss'
+import '@/scss/app.scss'
 import 'ant-design-vue/dist/antd.min.css';
-import isMobile from './plugins/is-mobile'
-import store from './store';
-import router from './router';
+import isMobile from '@/plugins/is-mobile'
+import store from '@/store';
+import router from '@/router';
 import '@/plugins/chartjs';
 import { registerGlobalComponents } from './components/global-components'
 import Antd from 'ant-design-vue/es'
-import App from './App.vue'
+import App from '@/App.vue'
 
 const app = createApp(App)
   .use(isMobile)
